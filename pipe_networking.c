905d6838b6ae4d796fcdd563ed85113fc7f700fd
Alan Chen <alanc94@nycstudents.net>
2024-12-18 18:22:59 -0500

    Client sends SYN and better format prints

--- pipe_networking.c
+++ pipe_networking.c
@@ -1,5 +1,8 @@
 #include "pipe_networking.h"
 
+// WKP: Client to server
+// PP: Server to client
+
 // UPSTREAM = to the server / from the client
 // DOWNSTREAM = to the client / from the server
 /*=========================
@@ -11,17 +14,21 @@
   returns the file descriptor for the upstream pipe.
   =========================*/
 int server_setup() {
-    remove(WKP);
+    remove(WKP); // TEMP
 
+    // Make named pipe
     int mkfifo_ret = mkfifo(WKP, 0644);
     ASSERT(mkfifo_ret, "mkfifo")
 
-    printf("mkfifo done\n");
+    printf("[SERVER]: Created WKP (now waiting for connection)\n");
 
     int from_client = open(WKP, O_RDONLY, 0);
-    ASSERT(from_client, "Server create WKP")
+    ASSERT(from_client, "Server open WKP")
 
-    printf("open done\n");
+    printf("[SERVER]: Client connected\n");
+
+    // Client connected, so remove the pipe
+    remove(WKP);
 
     return from_client;
 }
@@ -36,9 +43,8 @@ int server_setup() {
   returns the file descriptor for the upstream pipe (see server setup).
   =========================*/
 int server_handshake(int *to_client) {
-    server_setup();
-
-    int from_client;
+    int from_client = server_setup();
+    printf("[SERVER]: From client FD: %d\n", from_client);
 
     return from_client;
 }
@@ -53,20 +59,33 @@ int server_handshake(int *to_client) {
   returns the file descriptor for the downstream pipe.
   =========================*/
 int client_handshake(int *to_server) {
-    // Make PP
+    // Create PID string, used as name of PP and value of SYN
     pid_t client_pid = getpid();
+    char pid_string[HANDSHAKE_BUFFER_SIZE];
+    snprintf(pid_string, sizeof(pid_string), "%d", client_pid);
 
-    char private_pipe_name[10];
-    snprintf(private_pipe_name, sizeof(private_pipe_name), "%d", client_pid);
+    remove(pid_string); // TEMP
 
-    int mkfifo_ret = mkfifo(private_pipe_name, 0666);
+    // Make PP
+    int mkfifo_ret = mkfifo(pid_string, 0644);
     ASSERT(mkfifo_ret, "Client create PP")
 
-    // Open WKP
-    int wkp = open(WKP, O_WRONLY, 0);
-    ASSERT(wkp, "Client access WKP")
+    printf("[CLIENT]: Created PP\n");
 
-    int from_server;
+    // Open WKP
+    int upstream = open(WKP, O_WRONLY, 0);
+    ASSERT(upstream, "Client open WKP")
+
+    *to_server = upstream;
+
+    // Write SYN
+    write(upstream, pid_string, sizeof(pid_string));
+
+    printf("[CLIENT]: Sent SYN\n");
+
+    int from_server = open(pid_string, O_RDONLY, 0);
+    ASSERT(from_server, "Client open PP")
+
     return from_server;
 }
 
